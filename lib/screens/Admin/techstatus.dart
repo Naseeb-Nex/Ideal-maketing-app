97539f2790a35fbdea9a852673ec429d8b1f3452
Naseeb-Nex <muhammednaseeb02@gmail.com>
2022-04-18 21:00:12 +0530

    Update

--- lib/screens/Admin/techstatus.dart
+++ lib/screens/Admin/techstatus.dart
@@ -37,86 +37,109 @@ class _TechstatusState extends State<Techstatus> {
       Scaffold(
         backgroundColor: Colors.transparent,
         body: SafeArea(
-          child:DefaultTabController(
-                      length: 3,
-          child: Column(
-            children: [
-              Container(
-                width: s.width,
-                height: s.height * 1 / 7,
-                decoration: const BoxDecoration(
-                  color: Colors.transparent,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        IconButton(
-                          icon: const Icon(
-                            Icons.arrow_back,
-                            color: white,
+          child: DefaultTabController(
+            length: 4,
+            child: Column(
+              children: [
+                Container(
+                  width: s.width,
+                  height: s.height * 1 / 7,
+                  decoration: const BoxDecoration(
+                    color: Colors.transparent,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          IconButton(
+                            icon: const Icon(
+                              Icons.arrow_back,
+                              color: white,
+                            ),
+                            onPressed: () => Navigator.pop(context),
                           ),
-                          onPressed: () => Navigator.pop(context),
-                        ),
-                        Text(
-                          "${widget.name}'s Status",
-                          style: const TextStyle(
-                            fontFamily: "Nunito",
-                            fontSize: 19,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+                          Text(
+                            "${widget.name}'s Status",
+                            style: const TextStyle(
+                              fontFamily: "Nunito",
+                              fontSize: 19,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
                           ),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Icon(
-                            Icons.person,
-                            color: Colors.transparent,
-                            size: 30,
+                          const Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Icon(
+                              Icons.person,
+                              color: Colors.transparent,
+                              size: 30,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    
-                      TabBar(
-                        tabs: [
-                          Tab(text: "1"),
-                          Tab(
-                            text: "2",
-                          ),
-                          Tab(
-                            text: "3",
-                          )
                         ],
                       ),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  height: double.infinity,
-                  width: MediaQuery.of(context).size.width,
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(40),
-                        topRight: Radius.circular(40)),
-                    color: newbg,
-                  ),
-                  child: TabBarView(
-                    children: [
-                      Container(width: 100, height: 100, color: white,),
-                      Container(width: 100, height: 100, color: Colors.redAccent,),
-                      Container(width: 100, height: 100, color: bluebg,),
+                      const TabBar(
+                        tabs: [
+                          Tab(
+                            text: "assigned",
+                          ),
+                          Tab(
+                            text: "completed",
+                          ),
+                          Tab(
+                            text: "pending",
+                          ),
+                          Tab(
+                            text: "processing",
+                          ),
+                        ],
+                      ),
                     ],
                   ),
                 ),
-              ),
-            ],
+                Expanded(
+                  child: Container(
+                    height: double.infinity,
+                    width: MediaQuery.of(context).size.width,
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          topRight: Radius.circular(40)),
+                      color: newbg,
+                    ),
+                    
+                    child: TabBarView(
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          color: white,
+                        ),
+                        Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.redAccent,
+                        ),
+                        Container(
+                          width: 100,
+                          height: 100,
+                          color: bluebg,
+                        ),
+                        Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.blue,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),),
+        ),
       )
     ]);
   }

9d8ba957f314e767cb835689d14c2b06eea15eff
Naseeb-Nex <muhammednaseeb02@gmail.com>
2022-05-01 00:03:20 +0530

    Working on it

--- lib/screens/Admin/editprofile.dart
+++ lib/screens/Admin/editprofile.dart
@@ -18,10 +18,19 @@ class Editprofile extends StatefulWidget {
 }
 
 class _EditprofileState extends State<Editprofile> {
-
   Profile profile = Profile();
-
   FirebaseFirestore fb = FirebaseFirestore.instance;
+  // form
+  final _formKey = GlobalKey<FormState>();
+
+  // editing controller
+  final TextEditingController nameController = new TextEditingController();
+  final TextEditingController usernameController = new TextEditingController();
+  final TextEditingController designationController =
+      new TextEditingController();
+  final TextEditingController phn1Controller = new TextEditingController();
+  final TextEditingController phn2Controller = new TextEditingController();
+  final TextEditingController locationController = new TextEditingController();
 
   @override
   void initState() {
@@ -34,6 +43,135 @@ class _EditprofileState extends State<Editprofile> {
   @override
   Widget build(BuildContext context) {
     Size s = MediaQuery.of(context).size;
+
+    final namefield = TextFormField(
+      autofocus: false,
+      controller: nameController,
+      keyboardType: TextInputType.name,
+      onSaved: (value) {
+        nameController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Feild Cannot be empty");
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        labelText: "Full Name",
+        labelStyle: TextStyle(fontFamily: "Nunito", fontSize: 14),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+    );
+
+    //designation field
+    final designationfield = TextFormField(
+      autofocus: false,
+      controller: designationController,
+      keyboardType: TextInputType.text,
+      onSaved: (value) {
+        designationController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please enter your designation");
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        labelText: "Designation",
+        labelStyle: TextStyle(fontFamily: "Nunito", fontSize: 14),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+    );
+
+    //phn2 field
+    final phn2field = TextFormField(
+      autofocus: false,
+      controller: phn2Controller,
+      keyboardType: TextInputType.phone,
+      onSaved: (value) {
+        phn2Controller.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please Enter Your Personal Phone Number");
+        } else if (value.length != 10) {
+          return ("invalid Phone Number");
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        labelText: "Phone Number 2",
+        labelStyle: TextStyle(fontFamily: "Nunito", fontSize: 14),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+    );
+
+    //phn1 field
+    final phn1field = TextFormField(
+      autofocus: false,
+      controller: phn1Controller,
+      keyboardType: TextInputType.phone,
+      onSaved: (value) {
+        phn1Controller.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please enter your Phone Number");
+        } else if (value.length != 10) {
+          return ("invalid Phone Number");
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        labelText: "Phone Number 1",
+        labelStyle: TextStyle(fontFamily: "Nunito", fontSize: 14),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+    );
+
+    //location field
+    final locationfield = TextFormField(
+      autofocus: false,
+      controller: locationController,
+      keyboardType: TextInputType.text,
+      onSaved: (value) {
+        locationController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please Enter Your Home Location");
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        labelText: "Home Location",
+        labelStyle: TextStyle(fontFamily: "Nunito", fontSize: 14),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+    );
+
     return Scaffold(
       backgroundColor: primarybg,
       body: SafeArea(
@@ -53,7 +191,64 @@ class _EditprofileState extends State<Editprofile> {
                       ),
                       onPressed: () => Navigator.pop(context),
                     ),
+                    const Text(
+                      "Edit Profile",
+                      style: TextStyle(
+                          fontFamily: "Montserrat",
+                          color: bluebg,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold),
+                    )
                   ],
+                ),
+              ),
+              const SizedBox(height: 30),
+              Center(
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),border: Border.all(color: bluebg,),),
+                  child: Text(
+                    "${widget.name}'s Profile",
+                    style: const TextStyle(
+                        fontFamily: "Montserrat",
+                        color: bluebg,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 35,
+                      ),
+                      namefield,
+                      const SizedBox(
+                        height: 35,
+                      ),
+                      designationfield,
+                      const SizedBox(
+                        height: 35,
+                      ),
+                      phn1field,
+                      const SizedBox(
+                        height: 35,
+                      ),
+                      phn2field,
+                      const SizedBox(
+                        height: 35,
+                      ),
+                      locationfield,
+                      SizedBox(
+                        height: 35,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
@@ -63,6 +258,5 @@ class _EditprofileState extends State<Editprofile> {
     );
   }
 
-  setupsrc() async {
-  }
+  setupsrc() async {}
 }

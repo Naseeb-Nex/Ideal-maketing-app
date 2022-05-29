5d7cbf68fc44a82cba3c7eed730151f8713d108e
Naseeb-Nex <muhammednaseeb02@gmail.com>
2022-05-30 00:03:17 +0530

    update

--- lib/screens/Technician/completedsrc.dart
+++ lib/screens/Technician/completedsrc.dart
@@ -647,3 +647,4 @@ class CustomeAlertbx extends StatelessWidget {
     );
   }
 }
+
--- lib/screens/loginsrc.dart
+++ lib/screens/loginsrc.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:ideal_marketing/constants/constants.dart';
 
 import 'homeWrapper.dart';
 
@@ -16,14 +19,15 @@ class _LoginSrcState extends State<LoginSrc> {
   final _formKey = GlobalKey<FormState>();
 
   // editing controller
-  final TextEditingController emailController = new TextEditingController();
-  final TextEditingController passwordController = new TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   // firebase
   final _auth = FirebaseAuth.instance;
-  
+
   // string for displaying the error Message
   String? errorMessage;
+  bool load = false;
 
   @override
   Widget build(BuildContext context) {
@@ -48,8 +52,8 @@ class _LoginSrcState extends State<LoginSrc> {
         },
         textInputAction: TextInputAction.next,
         decoration: InputDecoration(
-          prefixIcon: Icon(Icons.mail),
-          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          prefixIcon: const Icon(Icons.mail),
+          contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
           hintText: "Email",
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
@@ -62,7 +66,7 @@ class _LoginSrcState extends State<LoginSrc> {
         controller: passwordController,
         obscureText: true,
         validator: (value) {
-          RegExp regex = new RegExp(r'^.{6,}$');
+          RegExp regex = RegExp(r'^.{6,}$');
           if (value!.isEmpty) {
             return ("Password is required for login");
           }
@@ -75,8 +79,8 @@ class _LoginSrcState extends State<LoginSrc> {
         },
         textInputAction: TextInputAction.done,
         decoration: InputDecoration(
-          prefixIcon: Icon(Icons.vpn_key),
-          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          prefixIcon: const Icon(Icons.vpn_key),
+          contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
           hintText: "Password",
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
@@ -88,12 +92,12 @@ class _LoginSrcState extends State<LoginSrc> {
       borderRadius: BorderRadius.circular(30),
       color: Colors.redAccent,
       child: MaterialButton(
-          padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
           minWidth: MediaQuery.of(context).size.width,
           onPressed: () {
             signIn(emailController.text, passwordController.text);
           },
-          child: Text(
+          child: const Text(
             "Login",
             textAlign: TextAlign.center,
             style: TextStyle(
@@ -104,32 +108,45 @@ class _LoginSrcState extends State<LoginSrc> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(36.0),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    SizedBox(
-                        height: 100,
-                        child: Image.asset(
-                          "assets/icons/imaicon.png",
-                          fit: BoxFit.contain,
-                        )),
-                    SizedBox(height: 45),
-                    emailField,
-                    SizedBox(height: 25),
-                    passwordField,
-                    SizedBox(height: 35),
-                    loginButton,
-                    SizedBox(height: 15),
-                  ],
-                ),
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Form(
+              key: _formKey,
+              child: Stack(
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: Center(
+                      child: load
+                          ? const CircularProgressIndicator(
+                              color: cheryred,
+                            )
+                          : null,
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                          height: 100,
+                          child: Image.asset(
+                            "assets/icons/imaicon.png",
+                            fit: BoxFit.contain,
+                          )),
+                      const SizedBox(height: 45),
+                      emailField,
+                      const SizedBox(height: 25),
+                      passwordField,
+                      const SizedBox(height: 35),
+                      loginButton,
+                      const SizedBox(height: 15),
+                    ],
+                  ),
+                ],
               ),
             ),
           ),
@@ -141,6 +158,9 @@ class _LoginSrcState extends State<LoginSrc> {
   // login function
   void signIn(String email, String password) async {
     if (_formKey.currentState!.validate()) {
+      setState(() {
+        load = true;
+      });
       try {
         await _auth
             .signInWithEmailAndPassword(email: email, password: password)
@@ -149,7 +169,13 @@ class _LoginSrcState extends State<LoginSrc> {
                   Navigator.of(context).pushReplacement(
                       MaterialPageRoute(builder: (context) => HomeWrapper())),
                 });
+        setState(() {
+          load = false;
+        });
       } on FirebaseAuthException catch (error) {
+        setState(() {
+          load = false;
+        });
         switch (error.code) {
           case "invalid-email":
             errorMessage = "Your email address appears to be malformed.";
@@ -171,7 +197,7 @@ class _LoginSrcState extends State<LoginSrc> {
             errorMessage = "Signing in with Email and Password is not enabled.";
             break;
           default:
-            errorMessage = "An undefined Error happened.";
+            errorMessage = "Something went wrong. Please check your internet connection?";
         }
         Fluttertoast.showToast(msg: errorMessage!);
         print(error.code);

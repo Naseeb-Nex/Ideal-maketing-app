7c62927edf238d672806a71c7aee02d709617508
Naseeb-Nex <muhammednaseeb02@gmail.com>
2022-10-31 19:50:23 +0530

    imports orginized, activity wrapper seperated and imported

--- lib/screens/Admin/techreportsrc.dart
+++ lib/screens/Admin/techreportsrc.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:ideal_marketing/components/dailycompletedcard.dart';
-import 'package:ideal_marketing/components/dailypendingcard.dart';
-import 'package:ideal_marketing/components/dailyprocessingcard.dart';
-import 'package:ideal_marketing/components/monthly_status.dart';
 import 'package:intl/intl.dart';
+// activity wrapper
+import 'package:ideal_marketing/screens/Admin/wrapper/activity_wrapper.dart';
+import 'package:ideal_marketing/components/monthly_status.dart';
 
+// icons
 import 'package:iconsax/iconsax.dart';
-
-import 'package:ideal_marketing/components/dailyassignedcard.dart';
-
+// loading_indicator
 import 'package:loading_indicator/loading_indicator.dart';
+// bottom navigation
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 // date Picker
 import 'package:flutter_rounded_date_picker/flutter_rounded_date_picker.dart';
@@ -71,13 +70,13 @@ class _TechreportsrcState extends State<Techreportsrc> {
   String? _selectedyear;
   String? _selectedmonth;
   String? _selectedday;
-  
+
   // serach controller
   final searchController = TextEditingController();
 
   // Montly List
-    List montly_filtered_rp = [];
-    List montly_search_rs = [];
+  List montly_filtered_rp = [];
+  List montly_search_rs = [];
 
   @override
   Widget build(BuildContext context) {
@@ -1524,8 +1523,10 @@ class _TechreportsrcState extends State<Techreportsrc> {
                                                       .toList();
                                                 }
 
-                                                if( searchController.text.isEmpty){
-                                                  montly_search_rs = montly_filtered_rp;
+                                                if (searchController
+                                                    .text.isEmpty) {
+                                                  montly_search_rs =
+                                                      montly_filtered_rp;
                                                 }
                                                 return Column(
                                                   children: [
@@ -1809,6 +1810,14 @@ class _TechreportsrcState extends State<Techreportsrc> {
                                                         techname:
                                                             montly_search_rs[i]
                                                                 ['techname'],
+                                                        more:
+                                                            montly_search_rs[i]
+                                                                ['more'],
+                                                        month:
+                                                            montly_search_rs[i]
+                                                                ['month'],
+                                                        day: montly_search_rs[i]
+                                                            ['day'],
                                                       ),
                                                       SizedBox(
                                                         height: 10,
@@ -1917,152 +1926,6 @@ class _TechreportsrcState extends State<Techreportsrc> {
           hintStyle: TextStyle(color: Colors.blueGrey),
         ),
       ),
-    );
-  }
-}
-
-// ignore: must_be_immutable
-class Activitywrapper extends StatelessWidget {
-  String? name;
-  String? address;
-  String? loc;
-  String? phn;
-  String? pgm;
-  String? chrg;
-  String? type;
-  String? upDate;
-  String? upTime;
-  String? docname;
-  String? status;
-  String? username;
-  String? techname;
-  String? assignedtime;
-  String? assigneddate;
-  String? priority;
-  String? camount;
-  String? remarks;
-  String? ctime;
-  String? cdate;
-  String? ccollname;
-  String? cdocname;
-  String? custdocname;
-  String? rpdocname;
-
-  Activitywrapper({
-    Key? key,
-    this.name,
-    this.address,
-    this.loc,
-    this.phn,
-    this.pgm,
-    this.chrg,
-    this.type,
-    this.upDate,
-    this.upTime,
-    this.docname,
-    this.status,
-    this.username,
-    this.techname,
-    this.assignedtime,
-    this.assigneddate,
-    this.priority,
-    this.camount,
-    this.remarks,
-    this.cdate,
-    this.ctime,
-    this.ccollname,
-    this.cdocname,
-    this.custdocname,
-    this.rpdocname,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    if (status == "completed") {
-      return Dailycompltedcard(
-        name: name,
-        address: address,
-        loc: loc,
-        phn: phn,
-        pgm: pgm,
-        chrg: chrg,
-        type: type,
-        upDate: upDate,
-        upTime: upTime,
-        docname: docname,
-        status: status,
-        username: username,
-        techname: techname,
-        assignedtime: assignedtime,
-        assigneddate: assigneddate,
-        priority: priority,
-        camount: camount,
-        ctime: ctime,
-        remarks: remarks,
-      );
-    } else if (status == "pending") {
-      return Dailypendingcard(
-        name: name,
-        address: address,
-        loc: loc,
-        phn: phn,
-        pgm: pgm,
-        chrg: chrg,
-        type: type,
-        upDate: upDate,
-        upTime: upTime,
-        docname: docname,
-        status: status,
-        username: username,
-        techname: techname,
-        assignedtime: assignedtime,
-        assigneddate: assigneddate,
-        priority: priority,
-        remarks: remarks,
-        pdate: cdate,
-        ptime: ctime,
-      );
-    } else if (status == "processing") {
-      return Dailyprocessingcard(
-        name: name,
-        address: address,
-        loc: loc,
-        phn: phn,
-        pgm: pgm,
-        chrg: chrg,
-        type: type,
-        upDate: upDate,
-        upTime: upTime,
-        docname: docname,
-        status: status,
-        username: username,
-        techname: techname,
-        assignedtime: assignedtime,
-        assigneddate: assigneddate,
-        priority: priority,
-        ptime: ctime,
-        pdate: cdate,
-        remarks: remarks,
-      );
-    }
-    return Dailyassignedcard(
-      name: name,
-      address: address,
-      loc: loc,
-      phn: phn,
-      pgm: pgm,
-      chrg: chrg,
-      type: type,
-      upDate: upDate,
-      upTime: upTime,
-      docname: docname,
-      status: status,
-      username: username,
-      techname: techname,
-      assignedtime: assignedtime,
-      assigneddate: assigneddate,
-      priority: priority,
-      custdocname: custdocname,
     );
   }
 }
